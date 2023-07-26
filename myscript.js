ba16405f2736fa556bcb394aa01cb204bf8f5b1c
maxlee07 <87029818+maxlee07@users.noreply.github.com>
2023-07-26 09:27:54 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var t="<div class='eff'>"+"Web Devlopment"+"<br>"+"UI degin"+"<br>"
+    +"Data Analysis"+"<br>"
+    +"Python programing"+"<br>"
+    +"C++"+"<br>"
+    +"</div>"
+    document.getElementById("demo1").innerHTML=t; 
+  }
+function myFunction1() {
+    var t="<div class='eff'>"+"Visual Studio"+"<br>"+"BootStarp"+"<br>"
+    +"React JS"+"<br>"
+    +"Audrino IDE"+"<br>"
+    +"Panda"+"<br>"
+    +"</div>"
+    document.getElementById("demo1").innerHTML=t; 
+}
+function myFunction2() {
+    var t='<a href="https://drive.google.com/file/d/1JLIfNClrAAf45a8JAZLqm88QQUArd1od/view?usp=drive_link" target="blank">'+'<i class="fa-solid fa-download  fa-2xl eff" style="color: #ff0000;"></i>'+'</a>'
+    document.getElementById("demo1").innerHTML =t;
+}
+
